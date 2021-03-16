987c3f39ef65c2d8f5698cd0e9fb362fc5f27229
Myles <mylespollie@gmail.com>
2021-03-16 17:27:37 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/scripts', express.static(__dirname + '/node_modules/paper/dist/'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/icons', express.static(__dirname + '/public/icons'));
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>
+    res.render('index')
+)
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
